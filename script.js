c68f82cbb590280cc8bad4817adaaa3f38cd17a6
HeadPnC <boskepnc@gmail.com>
2022-02-11 18:04:13 +0100

    new features

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt to select media stream, pass to video element, then play
+// Prompt to select media stream, pass to video element, then play and change the button text and style
 const selectMediaStream = async () => {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoElement.srcObject = mediaStream;
         videoElement.onloadedmetadata = () => {
             videoElement.play();
-        }
+        }        
+        button.innerText = "START";
+        button.classList.replace('default-button', 'start-button');
     } catch (error) {
         //Catch Error Here
     }
 }
 
-button.addEventListener('click', async () => {
-    // Disable Button;
-    button.disabled = true;
-    //Start Picture in Picture
+// Start Picture in Picture and change the button text and style
+const startSharing = async () => {    
     await videoElement.requestPictureInPicture();
-    // Reset Button
-    button.disabled = false;
-});
+    button.innerText = "STOP";
+    button.classList.replace('start-button', 'stop-button');
+}
 
-// On Load
-selectMediaStream();
+//Stop Picture in Picture or just change the button text and style if it is already stopped
+const stopSharing = () => {
+    if(document.pictureInPictureElement) document.exitPictureInPicture();
+    videoElement.srcObject = null;
+    button.innerText = "SELECT MEDIA";
+    button.classList.replace('stop-button', 'default-button');
+}
+
+// Check if media is selected or playing
+const checkMedia = () => {
+    if(!videoElement.srcObject) selectMediaStream();
+    else if (!document.pictureInPictureElement) startSharing();
+    else stopSharing();
+}
+
+// If Picture in Picture is stopped, change the button text and style
+videoElement.addEventListener('leavepictureinpicture', stopSharing)
+
+button.addEventListener('click', checkMedia);
